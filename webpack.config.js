4dd290accc185db61b9a590eb1bc8cf12d4c3bac
tamas1994 <donghaifeng@hotmail.com>
2018-04-10 22:24:17 +0800

    配置webpack-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
 		filename: "bundle.js"//打包后输出文件的文件名
 	},
 	devServer: {
-    contentBase: "./public",//本地服务器所加载的页面所在的目录
-    historyApiFallback: true,//不跳转
-    inline: true//实时刷新
-  } 
+	    contentBase: "./public",//本地服务器所加载的页面所在的目录
+	    historyApiFallback: true,//不跳转
+	    inline: true//实时刷新
+  	} 
 }
